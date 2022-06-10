41f893491d06edb9463c36f542fe6553eaa4bdbd
ninieell <ninieellp@gmail.com>
2022-06-10 20:16:51 +0400

    .

--- assets/jss/main.js
+++ assets/jss/main.js
@@ -2,23 +2,21 @@ var button = document.getElementById("button"),
 count = 0;
 var counter= 1;
 button.onclick = function (){
-    
   if (count<=9 && counter==1) {
     count += 1;
     button.innerHTML = "click me here" + " " + count;
   }
   else if (count>9 || counter==0) {
 
-    if (count==0)
-    {
+    if (count==0){
         counter=1;
         count += 1;
         button.innerHTML = "click me here"  + " " + count;
     }
     else{
-    counter=0;
-    count -= 1;
-    button.innerHTML = "click me here"  + " " + count;
+      counter=0;
+      count -= 1;
+      button.innerHTML = "click me here"  + " " + count;
     }
   }
     
